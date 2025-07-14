013b834530bacc170155183b3c8fc201f33d70ad
Kim <fopc4131@gmail.com>
2025-07-14 10:08:17 +0900

    feat(model): Debt 모델 및 Hive 어댑터 생성

---
+++ lib/models/debt.dart
@@ -0,0 +1,104 @@
+import 'package:hive/hive.dart';
+import 'package:naedonnaenwa/models/payment_history.dart';
+
+part 'debt.g.dart';
+
+@HiveType(typeId: 2)
+enum DebtType {
+  @HiveField(0)
+  lent, // 내가 빌려준 돈
+  @HiveField(1)
+  borrowed, // 내가 빌린 돈
+}
+
+@HiveType(typeId: 3)
+enum RecurringType {
+  @HiveField(0)
+  none,
+  @HiveField(1)
+  weekly,
+  @HiveField(2)
+  monthly,
+}
+
+@HiveType(typeId: 4)
+enum CurrencyType {
+  @HiveField(0)
+  krw, // 원화 ₩
+  @HiveField(1)
+  usd, // 달러 $
+  @HiveField(2)
+  jpy, // 엔화 ¥
+  @HiveField(3)
+  eur, // 유로 €
+  @HiveField(4)
+  other,
+}
+
+@HiveType(typeId: 5)
+class Debt {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String name;
+
+  @HiveField(2)
+  final int totalAmount;
+
+  @HiveField(3)
+  final List<PaymentHistory> payments;
+
+  @HiveField(4)
+  final DateTime dueDate;
+
+  @HiveField(5)
+  final String? note;
+
+  @HiveField(6)
+  final DebtType type;
+
+  @HiveField(7)
+  final RecurringType recurring;
+
+  @HiveField(8)
+  final bool isPaid;
+
+  @HiveField(9)
+  final DateTime createdAt;
+
+  @HiveField(10)
+  final DateTime updatedAt;
+
+  @HiveField(11)
+  final CurrencyType currency;
+
+  Debt({
+    required this.id,
+    required this.name,
+    required this.totalAmount,
+    required this.payments,
+    required this.dueDate,
+    required this.type,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.currency,
+    this.note,
+    this.recurring = RecurringType.none,
+    this.isPaid = false,
+  });
+
+  int get remainingAmount {
+    int result = totalAmount;
+
+    for (final p in payments) {
+      if (p.type == PaymentType.repayment) {
+        result -= p.amount;
+      } else if (p.type == PaymentType.borrowMore) {
+        result += p.amount;
+      }
+    }
+
+    return result;
+  }
+}
---
+++ lib/models/debt.g.dart
@@ -0,0 +1,211 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'debt.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class DebtAdapter extends TypeAdapter<Debt> {
+  @override
+  final int typeId = 5;
+
+  @override
+  Debt read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Debt(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      totalAmount: fields[2] as int,
+      payments: (fields[3] as List).cast<PaymentHistory>(),
+      dueDate: fields[4] as DateTime,
+      type: fields[6] as DebtType,
+      createdAt: fields[9] as DateTime,
+      updatedAt: fields[10] as DateTime,
+      currency: fields[11] as CurrencyType,
+      note: fields[5] as String?,
+      recurring: fields[7] as RecurringType,
+      isPaid: fields[8] as bool,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Debt obj) {
+    writer
+      ..writeByte(12)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.totalAmount)
+      ..writeByte(3)
+      ..write(obj.payments)
+      ..writeByte(4)
+      ..write(obj.dueDate)
+      ..writeByte(5)
+      ..write(obj.note)
+      ..writeByte(6)
+      ..write(obj.type)
+      ..writeByte(7)
+      ..write(obj.recurring)
+      ..writeByte(8)
+      ..write(obj.isPaid)
+      ..writeByte(9)
+      ..write(obj.createdAt)
+      ..writeByte(10)
+      ..write(obj.updatedAt)
+      ..writeByte(11)
+      ..write(obj.currency);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DebtAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class DebtTypeAdapter extends TypeAdapter<DebtType> {
+  @override
+  final int typeId = 2;
+
+  @override
+  DebtType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return DebtType.lent;
+      case 1:
+        return DebtType.borrowed;
+      default:
+        return DebtType.lent;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, DebtType obj) {
+    switch (obj) {
+      case DebtType.lent:
+        writer.writeByte(0);
+        break;
+      case DebtType.borrowed:
+        writer.writeByte(1);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is DebtTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class RecurringTypeAdapter extends TypeAdapter<RecurringType> {
+  @override
+  final int typeId = 3;
+
+  @override
+  RecurringType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return RecurringType.none;
+      case 1:
+        return RecurringType.weekly;
+      case 2:
+        return RecurringType.monthly;
+      default:
+        return RecurringType.none;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, RecurringType obj) {
+    switch (obj) {
+      case RecurringType.none:
+        writer.writeByte(0);
+        break;
+      case RecurringType.weekly:
+        writer.writeByte(1);
+        break;
+      case RecurringType.monthly:
+        writer.writeByte(2);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is RecurringTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class CurrencyTypeAdapter extends TypeAdapter<CurrencyType> {
+  @override
+  final int typeId = 4;
+
+  @override
+  CurrencyType read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return CurrencyType.krw;
+      case 1:
+        return CurrencyType.usd;
+      case 2:
+        return CurrencyType.jpy;
+      case 3:
+        return CurrencyType.eur;
+      case 4:
+        return CurrencyType.other;
+      default:
+        return CurrencyType.krw;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, CurrencyType obj) {
+    switch (obj) {
+      case CurrencyType.krw:
+        writer.writeByte(0);
+        break;
+      case CurrencyType.usd:
+        writer.writeByte(1);
+        break;
+      case CurrencyType.jpy:
+        writer.writeByte(2);
+        break;
+      case CurrencyType.eur:
+        writer.writeByte(3);
+        break;
+      case CurrencyType.other:
+        writer.writeByte(4);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CurrencyTypeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
